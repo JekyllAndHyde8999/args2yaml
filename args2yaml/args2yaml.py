4bd948f9ea67d0a1626d6e8c979413a77b511f6d
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2024-07-11 10:32:29 -0400

    added type hints and docstring

--- args2yaml/args2yaml.py
+++ args2yaml/args2yaml.py
@@ -1,7 +1,16 @@
+import argparse
+
 import yaml
 
 
-def args2yaml(args, *, file="opt.yaml"):
+def args2yaml(args: argparse.Namespace, *, file: str = "opt.yaml") -> None:
+    """
+    Parameters:
+        args: an argument namespace object containing your script's arguments
+        file: a string; the name of the yaml file where the arguments will be saved [default: opt.yaml]
+    Returns:
+        None
+    """
     # add file extension
     if not (file.endswith(".yaml") or file.endswith(".yml")):
         file += ".yaml"
